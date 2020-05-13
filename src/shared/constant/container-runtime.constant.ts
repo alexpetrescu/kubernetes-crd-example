928359cb9e626f6e9cd009118106badb8b381907
Alex Petrescu <alexpetrescu1996@gmail.com>
2020-05-13 21:29:58 +0300

    updated crd group

--- src/shared/constant/container-runtime.constant.ts
+++ src/shared/constant/container-runtime.constant.ts
@@ -2,7 +2,7 @@ export class ContainerRuntimeConstant {
   static readonly NAME: string = "container-runtime";
   static readonly CONTROLLER_NAME: string = `${ContainerRuntimeConstant.NAME}-controller`;
   static readonly CONTROLLER_NAMESPACE: string = "*";
-  static readonly CRD_GROUP_NAME: string = "kubeblocks.io";
+  static readonly CRD_GROUP_NAME: string = "alexpetrescu.io";
   static readonly CRD_GROUP_VERSION: string = "v1";
   static readonly CRD_NAME_PLURAL: string = "containerruntimes";
 }
